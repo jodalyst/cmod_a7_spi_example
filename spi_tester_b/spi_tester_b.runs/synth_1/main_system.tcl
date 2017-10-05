41584dca242275ec2ccfb03683aae0127d015c25
Joe <jodalyst@mit.edu>
2017-10-05 14:32:07 -0400

    adding in changes to clock

--- spi_tester_b/spi_tester_b.runs/synth_1/main_system.tcl
+++ spi_tester_b/spi_tester_b.runs/synth_1/main_system.tcl
@@ -3,8 +3,6 @@
 # 
 
 set_param xicom.use_bs_reader 1
-set_param tcl.collectionResultDisplayLimit 0
-set_msg_config -id {Common 17-41} -limit 10000000
 create_project -in_memory -part xc7a35tcpg236-1
 
 set_param project.singleFileAddWarning.threshold 0
--- spi_tester_b/spi_tester_b.srcs/sources_1/new/main_system.v
+++ spi_tester_b/spi_tester_b.srcs/sources_1/new/main_system.v
@@ -26,7 +26,7 @@ module main_system(input wire sysclk,
     output led0_b, //blue led output
     output led0_g, //green led output
     output led0_r, //red led output
-    inout [8:0]pio //outputs
+    inout [12:0]pio //outputs
     );
     wire lite, ccs, dc, rst, tcs, si, so, sck;
     
@@ -34,19 +34,30 @@ module main_system(input wire sysclk,
     assign pio[6:0] = {sck,si,tcs,rst,dc,ccs,lite};
     assign so = pio[7];
     
+    reg dc_reg;
+    assign dc = dc_reg;
+    
     //make the main clock:
     reg [1:0] clock_counter;
+    reg [2:0] slow_clock_counter;
     wire clock_25mhz;
+    wire clock_125mhz;
     wire rst;
     wire fastclk;
     assign rst = 1'b0;
+    
+    reg rst_reg;
+    assign rst = rst_reg;
     
     clk_wiz_0 cw(.clk_out1(fastclk),.reset(rst), .clk_in1(sysclk));
     always @(posedge fastclk)begin
         if (clock_counter == 2'b11) clock_counter <= 2'b00;
         else clock_counter <= clock_counter +1;
+        if (slow_clock_counter==3'b111) slow_clock_counter <= 3'b000;
+        else slow_clock_counter <= slow_clock_counter +1;
     end
     assign clock_25mhz = &clock_counter;
+    assign clock_125mhz = &slow_clock_counter;
     assign led[1] = clock_25mhz;
     //led flashing "heartbeat" code...
     reg [23:0] r;
@@ -63,8 +74,9 @@ module main_system(input wire sysclk,
     assign led0_g =1'b1; //r[21]?1'b1:1'b0;   //12hz
     assign led0_b =r[22]?1'b1:1'b0;  //3Hz
     assign led[0] = r[23];
-    assign pio[8] = r[23];
-    
+    //assign pio[8] = r[23];
+    reg led_out_reg;
+    assign pio[8] = btn[1];
     //------------------------
     //----------------------
     //assign lite = 1'b0;
@@ -82,6 +94,7 @@ module main_system(input wire sysclk,
     reg [2:0] selection; //which device to pick
     reg [15:0] bytes_to_send; //number of bytes to send
     reg [7:0] data_to_send; //data to send out
+    wire [7:0] chip_selects; ///chip selects
     assign tcs = chip_selects[0]; //tft chip select
     assign ccs = chip_selects[1]; //card reader chip select
     
@@ -94,13 +107,20 @@ module main_system(input wire sysclk,
     .busy(spi_busy),
     .trigger(trigger));
     
-    
+    assign pio[12:9] = state;
     reg [3:0] state;
-    localparam IDLE = 4'h0,
+    localparam IDLE = 4'hA;
     localparam START1 = 4'h1;
-    localparam RUN1
+    localparam RUN1 = 4'h2;
+    localparam PAUSE1 = 4'h3;
+    localparam START2 = 4'h4;
+    localparam RUN2 = 4'h5;
+    localparam PAUSE2 = 4'h6;
+    localparam START3 = 4'h7;
+    localparam RUN3 = 4'h8;
+    localparam PAUSE3 = 4'h9;
     
-    reg [23:0] pause_counter
+    reg [23:0] pause_counter;
     
     localparam SWRESET = 8'h01; //software reset
     localparam SLPOUT = 8'h11; //sleep out
@@ -111,29 +131,34 @@ module main_system(input wire sysclk,
     localparam MADCTL = 8'h36; //axis control
     localparam COLMOD = 8'h3A; //colormode;
     always @(posedge fastclk)begin
-        if (clock_25mhz)begin
+        if (clock_125mhz)begin
             case(state)
                 IDLE: begin
-                    dc <= 1'b1;
+                    led_out_reg <= 1'b0;
+                    dc_reg <= 1'b1;
                     trigger <= 1'b0;
-                    rst <=1 1'b0;
+                    rst_reg <=1'b0;
                     if(btn[1] == 1)begin
                         selection <= 3'b0; //pick device 0
                         bytes_to_send <= 16'b1; //send one byte
                         data_to_send <= SWRESET;
                         state <= START1;
+                    end
                 end
                 START1: begin
                     trigger<=1'b1;
-                    state <= RUN1
+                    if (~new_data && spi_busy) begin
+                        state <= RUN1;
+                    end
+                    state <= RUN1;
                     pause_counter <= 24'b0;
                 end
                 RUN1:begin
                     trigger <=1'b0;
-                    if (data
+                    if (new_data) state <= PAUSE1;
                 end
                 PAUSE1:begin
-                    if (&pause_counter)begin
+                    if (&pause_counter)begin //wait until pause is done
                         state <= START2;
                         data_to_send <= SLPOUT;
                     end else begin
@@ -141,8 +166,38 @@ module main_system(input wire sysclk,
                     end
                 end
                 START2: begin
-                    trigger <=1 1'b1;
-                    state <= RUN2;
+                    trigger <=1'b1;
+                    if (~new_data && spi_busy) begin
+                        state <= RUN2;
+                    end
+                end
+                RUN2: begin
+                   trigger <=1'b0;
+                   pause_counter <= 24'b0;
+                   if (new_data) state <= PAUSE2;
+                   
+                end
+                PAUSE2: begin
+                    if (&pause_counter)begin //wait until pause is done
+                        state <= START3;
+                        data_to_send <= DISPON;
+                    end else begin
+                        pause_counter <= pause_counter +1;
+                    end
+                end
+                START3: begin
+                    trigger <=1'b1;
+                    if (~new_data & spi_busy) begin
+                        state <= RUN3;
+                    end
+                end
+                RUN3: begin
+                   trigger <=1'b0;
+                   pause_counter <= 24'b0;
+                   if (new_data) state <= IDLE; 
+                end
+                default:
+                    state <= IDLE;
             endcase
         end
     end
